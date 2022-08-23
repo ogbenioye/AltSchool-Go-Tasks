c15e6ad0025b09559e5c67867834ea6b6e3d9f1d
Oluwapelumi Oyenuga <oluwapelumioyenuga@gmail.com>
2022-08-23 12:40:06 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ogbenioye/class1
+
+go 1.16
---
+++ name.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func echoName() {
+	fullName := "Oluwapelumi Oyenuga"
+	fmt.Println(fullName)
+}
+func main() {
+	echoName()
+}
